688d64452cd6dc2c37abcb549b7e7e4fb2f3c4dc
Jos Koot <jos.koot@gmail.com>
2018-11-09 01:31:02 +0100

    Add files via upload

---
+++ N.rkt
@@ -0,0 +1,10 @@
+#lang racket
+
+; By Jacob J. A. Koot
+; Part of R.rkt. Documentation in R.scrbl.
+
+(provide
+ (rename-out
+  (exact-nonnegative-integer? N?)
+  (exact-positive-integer? N+?)
+  (exact-integer? Z?)))
